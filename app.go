cc6fceba1cc19e50f437596b7f13945eeb69d7ed
Ivan Jimenez <ivan.jimenez@service.nsw.gov.au>
2021-12-14 14:59:46 -0500

    Initial Commit

---
+++ app.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"rsc.io/quote/v4"
+)
+
+func main() {
+ 	fmt.Println(quote.Go())
+
+	 c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{"purchases", key, value)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else {
+			// The client will automatically try to recover from all errors.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	c.Close()
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module merchante/suggestion-engine
+
+go 1.16
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.8.2
+	rsc.io/quote/v4 v4.0.1
+)
